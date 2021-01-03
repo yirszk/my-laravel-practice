abe517a2a6e6cd644986c34dded480b11f171625
Yoichiro Suzuki <y.suzuki@hey.com>
2021-01-04 08:38:53 +0900

    Chapter7-3: pagination

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,14 +8,17 @@ use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use App\Models\Person;
 
 class HelloController extends Controller
 {
 
     public function index(Request $request)
     {
-        $items = DB::table('people')->get();
-        return view('hello.index', ['items' => $items]);
+        $sort = $request->sort;
+        $items = Person::orderBy($sort, 'asc')->paginate(5);
+        $param = ['items' => $items, 'sort' => $sort];
+        return view('hello.index', $param);
     }
 
     public function post(Request $request)
