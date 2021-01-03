363799c3890d739b44612ce83715b095aa6b1b96
Yoichiro Suzuki <y.suzuki@hey.com>
2021-01-03 13:58:13 +0900

    Chapter6-3: insert, update, delete

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,13 @@ Route::post('hello/edit','App\Http\Controllers\HelloController@update');
 Route::get('hello/del','App\Http\Controllers\HelloController@del');
 Route::post('hello/del','App\Http\Controllers\HelloController@remove');
 Route::get('hello/show','App\Http\Controllers\HelloController@show');
+
 Route::get('person','\App\Http\Controllers\PersonController@index');
 Route::get('person/find','\App\Http\Controllers\PersonController@find');
 Route::post('person/find','\App\Http\Controllers\PersonController@search');
-
+Route::get('person/add','\App\Http\Controllers\PersonController@add');
+Route::post('person/add','\App\Http\Controllers\PersonController@create');
+Route::get('person/edit','\App\Http\Controllers\PersonController@edit');
+Route::post('person/edit','\App\Http\Controllers\PersonController@update');
+Route::get('person/del','App\Http\Controllers\PersonController@del');
+Route::post('person/del','App\Http\Controllers\PersonController@remove');
